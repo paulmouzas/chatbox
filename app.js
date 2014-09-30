815574daa5c45cf8fd5090359d2daed06fd1b4bc
unknown <paul.mouzas@gmail.com>
2014-09-30 09:39:44 -0400

    add user list

--- app.js
+++ app.js
@@ -5,43 +5,26 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
-
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '\\index.html');
-
 });
 
-// usernames which are currently connected to the chat
-var usernames = {};
-var numUsers = 0;
 
 io.on('connection', function(socket){
-  var addedUser = false;
-  
-  
+
   socket.on('join', function(data) {
-  
     socket.username = data;
-    
-    //socket.use('username', username, function() {
-    //  socket.broadcast.emit('user_connected', username);
-    //  for (var sid in io.sockets.sockets) {
-    //    io.sockets.sockets[sid].get('username', function(err, username) {
-    //      socket.emit('user_connected', username);
-    //    });
-    //  }
-    console.log(socket.username)
+    socket.broadcast.emit('user_connected', socket.username);
+    for (var sid in io.sockets.sockets) {
+
   });
-  
+
   socket.on('text', function(msg) {
-    console.log('in here');
     io.emit('text', socket.username + ': ' + msg);
-
   });
-
 });
 
